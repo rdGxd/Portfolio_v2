4f458cbdc90722835b8c098f6a30f8083b1a5222
Rodrigo Silva <rdg.hora1@gmail.com>
2024-01-01 23:04:50 -0300

    GoTop Fixed

--- src/components/GoTop/index.tsx
+++ src/components/GoTop/index.tsx
@@ -1,7 +1,7 @@
 export const GoTop = () => {
   return (
     <a
-      href="/"
+      href="#"
       className="fixed z-10 flex items-center justify-center w-10 h-10 transition duration-300 ease-in-out delay-150 rounded opacity-50 bg-gray bottom-8 right-8 hover:bg-primary hover:-translate-y-1 hover:scale-110"
       aria-label="Go to top"
       title="Go to top"
